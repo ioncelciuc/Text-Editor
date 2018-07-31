1e025091d5045913c03f900fca58b97df5d3e43f
ioncelciuc <ioncelciuc2001@gmail.com>
2018-07-31 15:51:25 +0300

    update mainClass.java && update optiuni.java

--- src/optiuni.java
+++ src/optiuni.java
@@ -4,17 +4,45 @@ public class optiuni extends mainClass{
 	
 	Scanner input=new Scanner(System.in);
 	public int alegereOptiuni;
+	public String filePath;
+	
+	public void enter() {
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+	
+	public void goToMenu() {
+		enter();
+		System.out.println("Tasteaza \\ urmat de enter pentru a reveni la meniul principal");
+		filePath=input.next();
+		if(filePath.equals("\\")) {
+			enter();
+			runApp();
+		}
+		else {
+			enter();
+			goToMenu();
+		}
+	}
 	
 	public void runApp() {
 		fileReader reader=new fileReader();
 	    reader.fileRead("menu");
 	    alegereOptiuni=input.nextInt();
 	    if(alegereOptiuni==1) {
+	    	enter();
 	    	reader.fileRead("instructiuni");
-	    	runApp();
+	    	goToMenu();
 	    }
 	    else if(alegereOptiuni==2){
-	    	
+	    	enter();
+	    	reader.fileRead("instructiuni_deschidere_fisier");
+	    	enter();
+	    	filePath=input.next();
+	    	enter();
+	    	reader.fileRead(filePath);
+	    	goToMenu();
 	    }
 	}
 }
